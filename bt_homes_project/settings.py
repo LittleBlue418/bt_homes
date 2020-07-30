771a48dee29ba02eaa38f0c1e9f87f44d0e93733
LittleBlue418 <hollyathomas88@gmail.com>
2020-07-30 13:24:13 +0200

    Set up first app

--- bt_homes_project/settings.py
+++ bt_homes_project/settings.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'pages',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

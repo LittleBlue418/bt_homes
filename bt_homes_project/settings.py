86663d87d8fb5c7b79f6d8682854d26a390ec822
LittleBlue418 <hollyathomas88@gmail.com>
2020-08-05 14:02:11 +0200

    Adding set up for media folder

--- bt_homes_project/settings.py
+++ bt_homes_project/settings.py
@@ -142,3 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bt_homes_project/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'

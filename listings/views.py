6d382b5a6960aaea22fba35a3d3bb6f6cf380d9a
LittleBlue418 <hollyathomas88@gmail.com>
2020-08-06 12:59:42 +0200

    filter & pagination when listing propertiers from db

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
+
+"""
+    Returning all our house listings, ordered by the most recently
+    published, filtered to only show published properties.
+
+    Pagination used to display 6 to a page.
+"""
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
 
     return render(request, 'listings/listings.html', context)

bfc02c8344bd9998d089588a2dab44a172a745f4
LittleBlue418 <hollyathomas88@gmail.com>
2020-08-06 16:13:44 +0200

    Programatically populate form drop down boxes

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+from .choices import price_choices, bedroom_choices, state_choices
 from .models import Listing
 
 
@@ -18,7 +19,7 @@ def index(request):
     paged_listings = paginator.get_page(page)
 
     context = {
-        'listings': paged_listings
+        'listings': paged_listings,
     }
 
     return render(request, 'listings/listings.html', context)
@@ -38,4 +39,11 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'listings/search.html', context)

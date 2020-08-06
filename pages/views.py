9a4df015bc61f54e763ee97f936aa0c7714dd455
LittleBlue418 <hollyathomas88@gmail.com>
2020-08-06 13:34:57 +0200

    Listings from db for the home page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
+from listings.models import Listing
 
+
+"""
+    Using the Listings model to import listings for the front page.
+    Filtered by the most recent, only published, and only returning
+    3 listings.
+"""
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')

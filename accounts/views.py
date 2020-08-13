260577c8f980962e718d491d41c85e8aa6beb2ad
LittleBlue418 <hollyathomas88@gmail.com>
2020-08-13 13:37:59 +0200

    login + register methods, testing logic with 'print'

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, redirect
 
+"""
+    Log in user, redirect based on request method.
+"""
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        print('LOGGED IN')
+        return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
 
+
+"""
+    Register an account, redirect based on request method.
+"""
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        print('SUBMITTES REG')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
 
 def logout(request):
     return redirect('index.html')
